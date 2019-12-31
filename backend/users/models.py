658934f06445ae6a736cd91d756895550d326474
Illya Artemchuk <ilyalinkis@gmail.com>
2019-12-30 16:45:37 -0800

    Users now create teacher and student models on creation

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
 class User(AbstractUser):
     STUDENT = "ST"
     TEACHER = "TE"
@@ -10,7 +9,6 @@ class User(AbstractUser):
         (STUDENT, "Student"),
         (TEACHER, "Teacher")
     ]
-
 
     Role = models.CharField(
         max_length=2,
@@ -21,11 +19,21 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+    def save(self, *args, **kwargs):
+        created = not self.pk
+        super().save(*args, **kwargs)
+        if created and self.Role == "ST":
+            Student.objects.create(user=self)
+        elif created and self.Role == "TE":
+            Teacher.objects.create(user=self)
+
+
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
+
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
