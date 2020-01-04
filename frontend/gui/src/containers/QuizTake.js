acb8714d30099ba322a0cc42e9f4e760cd547c47
Illya Artemchuk <ilyalinkis@gmail.com>
2020-01-03 16:35:14 -0800

    QuizTake component still halfway done

--- frontend/gui/src/containers/QuizTake.js
+++ frontend/gui/src/containers/QuizTake.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import { Row, Col, Button, Card } from "antd";
+import { Row, Col, Button, Card, Typography } from "antd";
 import { Radio } from "antd";
+const { Text } = Typography;
 
 class QuizTake extends React.Component {
   state = {
@@ -53,17 +54,42 @@ class QuizTake extends React.Component {
     return this.state.data.questions[questionNumber];
   };
 
+  //Controls the Start button
   takingQuizToggle = e => {
     this.state.takingQuiz
       ? this.setState({ takingQuiz: false })
       : this.setState({ takingQuiz: true });
   };
 
+  //adds your submitted answer to the state and progresses the quiz to the next question
+  handleNextQuestion = (question_number, choice_number) => {
+    const answer = {
+      question: question_number,
+      answer: choice_number
+    };
+    this.setState(
+      {
+        questionNumber: this.state.questionNumber + 1,
+        submittedAnswers: this.state.submittedAnswers.concat(answer),
+        multipleChoiceValue: 0
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  testClick = () => {
+    console.log("question number: " + this.state.questionNumber);
+    console.log("state data length" + this.state.data.questions.length);
+  };
+
   render() {
     const radioStyle = {
       display: "block",
-      height: "30px",
-      lineHeight: "30px"
+      textAlign: "left",
+      fontSize: "20px",
+      color: "rgb(46, 45, 45)"
     };
     return (
       <div>
@@ -96,35 +122,62 @@ class QuizTake extends React.Component {
                 </div>
               ) : (
                 <div>
-                  <Card
-                    title={`${this.state.questionNumber + 1}) ${
-                      this.handleQuestionNumber().content
-                    }`}
-                  >
-                    <Radio.Group
-                      value={this.state.multipleChoiceValue}
-                      onChange={this.onChoiceChange}
+                  <Card>
+                    <Text
+                      style={{
+                        fontSize: "20px",
+                        color: "black",
+                        marginBottom: "25px"
+                      }}
                     >
-                      {this.handleQuestionNumber().choices.map(
-                        (choice, index) => (
-                          <Radio
-                            style={radioStyle}
-                            value={choice.choice_number}
-                            key={index}
-                          >
-                            {choice.text}
-                          </Radio>
-                        )
-                      )}
-                    </Radio.Group>
+                      {`${this.state.questionNumber + 1}) ${
+                        this.handleQuestionNumber().content
+                      }`}
+                    </Text>
+                    <Row>
+                      <Radio.Group
+                        value={this.state.multipleChoiceValue}
+                        onChange={this.onChoiceChange}
+                      >
+                        {this.handleQuestionNumber().choices.map(
+                          (choice, index) => (
+                            <Radio
+                              style={radioStyle}
+                              value={choice.choice_number}
+                              key={index}
+                            >
+                              {choice.text}
+                            </Radio>
+                          )
+                        )}
+                      </Radio.Group>
+                    </Row>
                   </Card>
-                  <Button
-                    type="primary"
-                    size="large"
-                    style={{ marginTop: "25px" }}
-                  >
-                    Next Question
-                  </Button>
+                  {this.state.data.questions.length >
+                  this.state.questionNumber + 1 ? (
+                    <Button
+                      type="primary"
+                      size="large"
+                      style={{ marginTop: "18px" }}
+                      onClick={() =>
+                        this.handleNextQuestion(
+                          this.handleQuestionNumber().question_number,
+                          this.state.multipleChoiceValue
+                        )
+                      }
+                    >
+                      Next Question
+                    </Button>
+                  ) : (
+                    <Button
+                      type="primary"
+                      onClick={() => this.testClick()}
+                      size="large"
+                    >
+                      {" "}
+                      Submit{" "}
+                    </Button>
+                  )}
                 </div>
               )}
             </Col>
